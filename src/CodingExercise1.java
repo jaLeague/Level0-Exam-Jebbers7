fa8a508275440937e7ab8f02b8f53bd8664d101c
jaLeague <jackie.talbot@jointheleague.org>
2021-02-02 16:12:04 -0800

    Initial commit

---
+++ src/CodingExercise1.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import org.jointheleague.graphical.robot.Robot;
+
+/* Level 0 Exam: Coding  Exercise #1 */
+public class CodingExercise1 {
+
+    public static void main(String[] args) {
+   	 // 3. ask the user what color they would like the Robot to draw
+
+   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+
+   	 // 2. set the pen width to 10
+
+   	 // 1. make the Robot draw a shape
+   	 
+    }
+
+}
---
+++ src/CodingExercise2.java
@@ -0,0 +1,17 @@
+
+
+/* Level 0 Exam: Coding  Exercise #2 */
+import javax.swing.JOptionPane;
+
+/**
+ * Write a program that asks the user for their age. 
+ * Tell them which year they were born. 
+ * If they are over 30, tell them they are too old to play this game.
+ **/
+
+public class CodingExercise2 {
+
+}
+
+
+
